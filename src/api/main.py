e1cc16e7b1af1834b0a29920f472e56b6a7e4fde
Martin Oywa <martinoywa2@gmail.com>
2024-07-31 02:06:38 +0300

    Add database module (#12)
    * Add database module
    
    * Update transcript functions and endpoints
    
    * Update transcript function
    
    * Add delete function

--- src/api/main.py
+++ src/api/main.py
@@ -6,11 +6,12 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
+from src.api.database.db import save_message, load_session_history, delete_session_history
 from src.api.models.models import AIResponse
 
 # AI Prompt Template and Chain
 print("Loading LLM")
-llm = Ollama(model="llama2")
+llm = Ollama(model="llama3.1")
 
 qa_system_prompt = """You are an assistant for question-answering tasks. \
 Use the following pieces of retrieved context to answer the question. \
@@ -25,16 +26,12 @@ qa_prompt = ChatPromptTemplate.from_messages([
 output_parser = StrOutputParser()
 
 chain = qa_prompt | llm | output_parser
-
-# Transcript
-processed_chat_history = []  # TODO fetch from session store
-transcript_memory = {}
 session_store = {}
 
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in session_store:
-        session_store[session_id] = ChatMessageHistory()
+        session_store[session_id] = load_session_history(session_id)
     return session_store[session_id]
 
 
@@ -51,24 +48,23 @@ router = APIRouter()
 prefix = "/api/v1"
 
 
-def process_chat_history(human, ai):
-    """
-    Function to process chat history for payload.
-    Addedd as cannot have Langchain type in pydantic v2.
-    May not be necessary as /chathistory endpoint returns data well.
-    :param human:
-    :param ai:
-    :return:
-    """
-    processed_chat_history.append((human, ai))
+def generate_transcript(session_id):  # TODO unable to load last messages, format different db values
+    transcript_memory = []
+    history = chain_with_history.get_session_history(session_id).messages
+    if len(history) > 0:
+        for message in history:
+            try:
+                transcript_memory.append(f"{message['role']}: {message['content']}")
+            except TypeError:
+                pass
+    return transcript_memory
 
 
-def update_transcript(session_id, text, role="Human:"):
-    if transcript_memory.get(session_id):
-        transcript_memory[session_id].append(f"{role} {text}")
-    else:
-        transcript_memory[session_id] = [f"{role} {text}"]
-
+# def update_transcript(session_id, text, role="Human:"):
+#     if transcript_memory.get(session_id):
+#         transcript_memory[session_id].append(f"{role} {text}")
+#     else:
+#         transcript_memory[session_id] = [f"{role} {text}"]
 
 @router.get("/")
 async def home():
@@ -77,42 +73,43 @@ async def home():
 
 @router.post("/chat", response_model=AIResponse)
 async def chat(message: str, session_id: str):
+    save_message(session_id, "human", message)
     response = chain_with_history.invoke(
         {"input": message},
         config={"configurable": {"session_id": session_id}}
     )
     if response[:3] == "AI:":  # LLM sometimes adds this.
         response = response[3:]
+    save_message(session_id, "ai", response)
 
     # update transcript
-    update_transcript(session_id, message)
-    update_transcript(session_id, response, role="AI Assisstant:")
+    # update_transcript(session_id, message)
+    # update_transcript(session_id, response, role="AI Assisstant:")
 
     return {
-            "response": response
+        "response": response
     }
 
 
 @router.get("/chathistory")
 async def chat_history(session_id: str):
-    return {"chat_history": session_store.get(session_id)}
+    return {"chat_history": chain_with_history.get_session_history(session_id)}
 
 
-@router.post("/deltechathisory")
+@router.delete("/deltechathisory/{session_id}")
 async def delete_chat_history(session_id: str):
-    del session_store[session_id]
-    return {"message": "Chat History Cleared"}
+    message = delete_session_history(session_id)
+    return {"message": message}
 
 
 @router.get("/transcript")
 async def transcript(session_id: str):
-    return {"transcript": transcript_memory.get(session_id)}
+    return {"transcript": generate_transcript(session_id)}
 
 
-@router.post("/deltetranscript")
+@router.delete("/deltetranscript")
 async def delete_transcript():
-    transcript_memory.clear()
-    return {"message": "Transcript Cleared"}
-
+    # transcript_memory.clear()
+    return {"message": "WIP! Nothing cleared"}
 
 app.include_router(router, prefix=prefix)
